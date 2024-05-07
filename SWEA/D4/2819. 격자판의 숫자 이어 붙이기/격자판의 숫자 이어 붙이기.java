18a6fb5bf37751d746e38aeabc4f83963eb65096
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-05-07 10:57:13 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 208 ms, Memory: 91,236 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.java
@@ -9,9 +9,10 @@ public class Solution {
         
         int totalTest = Integer.parseInt(reader.readLine());
         for (int test_case = 1; test_case <= totalTest; test_case++) {
-            int[][] grid = new int[4][4];
-            HashSet<String>[][] set = new HashSet[4][4];
+            int[][] grid = new int[4][4]; // 격자칸에 적혀있는 수
+            HashSet<String>[][] set = new HashSet[4][4]; // 격자칸마다 만들수 있는 수의 집합
             
+            // 초기화
             for (int row = 0; row < 4; row++) {
                 token = new StringTokenizer(reader.readLine());
                 for (int col = 0; col < 4; col++) {
@@ -21,10 +22,11 @@ public class Solution {
                 }
             }
             
+            // 주변 격자칸이 만들 수 있는 수 앞에 격자칸에 적혀있는 수를 붙여서 집합을 만들기
             for (int count = 1; count <= 6; count++) {
                 int[] dr = {1, -1, 0, 0};
                 int[] dc = {0, 0, 1, -1};
-                HashSet<String>[][] tmpset = new HashSet[4][4];
+                HashSet<String>[][] tmpset = new HashSet[4][4]; // 격자칸이 만들 수 있는 수를 저장하는 임시 집합
                 for (int row = 0; row < 4; row++) {
                     for (int col = 0; col < 4; col++) {
                         tmpset[row][col] = new HashSet<String>();
@@ -32,7 +34,7 @@ public class Solution {
                             int nr = row + dr[d];
                             int nc = col + dc[d];
                             
-                            if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) continue;
+                            if (nr < 0 || nc < 0 || nr >= 4 || nc >= 4) continue; // 범위 확인
                             for (String str : set[nr][nc]) {
                                 tmpset[row][col].add(String.valueOf(grid[row][col]) + str);
                             }
@@ -40,6 +42,7 @@ public class Solution {
                     }
                 }
                 
+                // 임시 집합을 복사
                 for (int row = 0; row < 4; row++) {
                     for (int col = 0; col < 4; col++) {
                         set[row][col] = tmpset[row][col];
