337603740d59d52c0950bb58591b5724fd5be8d6
PKafka <66552990+PKafka0320@users.noreply.github.com>
2024-08-30 02:07:21 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 132 ms, Memory: 24,032 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -32,25 +32,24 @@ public class Solution {
         for (int tc = 1; tc <= T; tc++) {
             answer.append("#").append(tc).append(" ");
             
-            int gridS = Integer.parseInt(br.readLine());
-            int[][] grid = new int[gridS][gridS];
+            int n = Integer.parseInt(br.readLine());
             positions = new Position[10];
             stairs = new Stair[2];
             pIndex = 0;
             result = Integer.MAX_VALUE;
             
             int sIndex = 0;
-            for (int r = 0; r < gridS; r++) {
+            for (int r = 0; r < n; r++) {
                 st = new StringTokenizer(br.readLine());
-                for (int c = 0; c < gridS; c++) {
-                    grid[r][c] = Integer.parseInt(st.nextToken());
+                for (int c = 0; c < n; c++) {
+                    int number = Integer.parseInt(st.nextToken());
                     
-                    if (grid[r][c] == 1) {
+                    if (number == 1) {
                         positions[pIndex++] = new Position(r, c);
                     }
                     
-                    if (grid[r][c] != 1 && grid[r][c] != 0) {
-                        stairs[sIndex++] = new Stair(r, c, grid[r][c]);
+                    if (number != 1 && number != 0) {
+                        stairs[sIndex++] = new Stair(r, c, number);
                     }
                 }
             }
@@ -75,69 +74,43 @@ public class Solution {
     }
     
     public static void evaluate(int[] selected) {
-        PriorityQueue<Integer> pq1 = new PriorityQueue<>();
-        PriorityQueue<Integer> pq2 = new PriorityQueue<>();
-        
-        for (int i = 0; i < pIndex; i++) {
-            if (selected[i] == 0) {
-                int preTime = Math.abs(positions[i].row - stairs[0].row) + Math.abs(positions[i].col - stairs[0].col) + 1;
-                pq1.add(preTime);
-            } else {
-                int preTime = Math.abs(positions[i].row - stairs[1].row) + Math.abs(positions[i].col - stairs[1].col) + 1;
-                pq2.add(preTime);
-            }
-        }
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         Queue<Integer> q = new LinkedList<>();
         int maxTime = 0;
         
-        while (!pq1.isEmpty()) {
-            int preTime = pq1.poll();
-            
-            if (q.size() < 3) {
-                q.add(preTime + stairs[0].time);
-            } else {
-                int time = q.poll();
-                while (!q.isEmpty() && q.peek() == time) {
-                    q.poll();
-                }
+        for (int stairIdx = 0; stairIdx < 2; stairIdx++) {
+            for (int i = 0; i < pIndex; i++) {
+                if (selected[i] != stairIdx) continue;
                 
-                if (preTime <= time) {
-                    q.add(time + stairs[0].time);
+                int preTime = Math.abs(positions[i].row - stairs[stairIdx].row) +
+                        Math.abs(positions[i].col - stairs[stairIdx].col) + 1;
+                pq.add(preTime);
+            }
+            
+            while (!pq.isEmpty()) {
+                int preTime = pq.poll();
+                
+                if (q.size() < 3) {
+                    q.add(preTime + stairs[stairIdx].time);
                 } else {
-                    q.add(preTime + stairs[0].time);
+                    int time = q.poll();
+                    while (!q.isEmpty() && q.peek() == time) {
+                        q.poll();
+                    }
+                    
+                    if (preTime <= time) {
+                        q.add(time + stairs[stairIdx].time);
+                    } else {
+                        q.add(preTime + stairs[stairIdx].time);
+                    }
                 }
             }
-        }
-        
-        while (!q.isEmpty()) {
-            maxTime = Math.max(maxTime, q.poll());
-        }
-        
-        while (!pq2.isEmpty()) {
-            int preTime = pq2.poll();
             
-            if (q.size() < 3) {
-                q.add(preTime + stairs[1].time);
-            } else {
-                int time = q.poll();
-                while (!q.isEmpty() && q.peek() == time) {
-                    q.poll();
-                }
-                
-                if (preTime <= time) {
-                    q.add(time + stairs[1].time);
-                } else {
-                    q.add(preTime + stairs[1].time);
-                }
+            while (!q.isEmpty()) {
+                maxTime = Math.max(maxTime, q.poll());
             }
-        }
-        
-        while (!q.isEmpty()) {
-            maxTime = Math.max(maxTime, q.poll());
         }
         
         result = Math.min(result, maxTime);
     }
-
 }
