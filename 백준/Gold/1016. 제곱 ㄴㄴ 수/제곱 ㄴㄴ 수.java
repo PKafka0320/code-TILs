76090e2b4ee8280a8bdc9062155ffd07b4a78ded
PKafka <66552990+PKafka0320@users.noreply.github.com>
2025-01-21 15:27:46 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 100 ms, Memory: 12588 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -2,31 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		long s = Long.parseLong(st.nextToken());
-		long e = Long.parseLong(st.nextToken());
-		
-		long count = e-s+1;
-		boolean[] check = new boolean[(int)count];
-		
-		for(long i=2; i*i<=e; i++) {
-			long pow = i*i; // 제곱수 
-			long tmp = s/pow;  // 제곱수로 나눠지나? 
-			if(s%pow != 0) { // 제곱수 아니면 +1 
-				tmp += 1;
-			}
-			
-			for(long j = tmp; j*pow<=e; j++) {
-				int canSqrt = (int)(j*pow-s);
-				if(!check[canSqrt]) {
-					check[canSqrt] = true; // 나누어 떨어지는 수 범위 초과로 -s 해서 저장
-					count--;
-				}
-			}
-		}
-		System.out.println(count);
-	}
+        st = new StringTokenizer(br.readLine());
+        long MIN = Long.parseLong(st.nextToken());
+        long MAX = Long.parseLong(st.nextToken());
+        long size = MAX - MIN + 1;
+        boolean[] notPrime = new boolean[(int) size];
+        long answer = size;
+
+        for (long i = 2; i * i <= MAX; i++) {
+            long power = i * i;
+            long temp = MIN / power;
+
+            if (MIN % power != 0) {
+                temp += 1;
+            }
+
+            for (long j = temp; j * power <= MAX; j++) {
+                int offset = (int) (j * power - MIN);
+                if (!notPrime[offset]) {
+                    notPrime[offset] = true;
+                    answer--;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
