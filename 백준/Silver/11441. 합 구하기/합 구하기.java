69dc87f9547655ab81ac99b4cd48535534c92aa4
PKafka <66552990+PKafka0320@users.noreply.github.com>
2025-04-24 00:10:47 +0900

    [Silver III] Title: 합 구하기, Time: 444 ms, Memory: 57212 KB -BaekjoonHub

---
+++ 백준/Silver/11441. 합 구하기/합 구하기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N+1];
+        int[] sum = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            sum[i] = sum[i-1] + arr[i];
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            answer.append(sum[j] - sum[i - 1]).append("\n");
+        }
+        System.out.println(answer);
+    }
+}
