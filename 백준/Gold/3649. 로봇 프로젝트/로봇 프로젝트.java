7484bc354be54114b6252b4e83c9309779604497
PKafka <66552990+PKafka0320@users.noreply.github.com>
2025-07-27 10:15:04 +0900

    [Gold V] Title: 로봇 프로젝트, Time: 1476 ms, Memory: 306552 KB -BaekjoonHub

--- 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
+++ 백준/Gold/3649. 로봇 프로젝트/로봇 프로젝트.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
     static int x, n;
-    static HashSet<Integer> blocks;
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -13,33 +12,40 @@ public class Main {
         while ((input = reader.readLine()) != null) {
             x = Integer.parseInt(input) * 10_000_000; // 구멍의 너비
             n = Integer.parseInt(reader.readLine()); // 레고 조각의 수
-            
-            blocks = new HashSet<>(); // 레고 조각의 길이와 개수
-            int maxDiff = -1; // 두 레고의 최대 길이 차이
-            int block1 = -1; // 짧은 레고의 길이
-            int block2 = -1; // 긴 레고의 길이
-            for (int idx = 0; idx < n; idx++) {
-                int block = Integer.parseInt(reader.readLine());
-                
-                int diff = x - block;
-                if (maxDiff >= Math.max(diff, block) - Math.min(diff, block)) continue; // 길이 차이 비교
 
-                if (blocks.contains(diff)) {
-                    block1 = Math.min(diff, block);
-                    block2 = Math.max(diff, block);
-                    maxDiff = block2 - block1;
-                }
-                
-                blocks.add(block); // 레고 추가
+            int[] blocks = new int[n];
+            for (int i = 0; i < n; i++) {
+                blocks[i] = Integer.parseInt(reader.readLine());
             }
-            
+
+            Arrays.sort(blocks); // 정렬
+
+            int left = 0;
+            int right = n - 1;
+            int block1 = -1;
+            int block2 = -1;
+
+            while (left < right) {
+                int sum = blocks[left] + blocks[right];
+
+                if (sum == x) {
+                    block1 = blocks[left];
+                    block2 = blocks[right];
+                    break;
+                } else if (sum < x) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+
             if (block1 != -1 && block2 != -1) {
                 answer.append("yes ").append(block1).append(" ").append(block2).append("\n");
-            }
-            else {
+            } else {
                 answer.append("danger\n");
             }
         }
-        System.out.println(answer);
+
+        System.out.print(answer);
     }
 }
