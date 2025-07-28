d762309ec5b6efaa71384275710f2d0902b6244e
PKafka <66552990+PKafka0320@users.noreply.github.com>
2025-07-28 18:50:02 +0900

    [Gold II] Title: 나누기, Time: 204 ms, Memory: 29428 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -6,7 +6,7 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N;
 	static int[] arr, sum;
-	static long[] dp;
+	static long[][] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +14,7 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		arr = new int[N + 1];
 		sum = new int[N + 1];
-		dp = new long[5];
+		dp = new long[N + 1][5]; // dp[i][j] = i 번째까지 j 번으로 나누는 경우의 수
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for (int i = 1; i <= N; i++) {
@@ -25,40 +25,25 @@ public class Main {
 			sum[i] = sum[i - 1] + arr[i];
 		}
 
-		// 누적합이 4의 배수가 아니라면 불가능
 		if (sum[N] % 4 != 0) {
 			System.out.println(0);
 			return;
 		}
 
-		long answer = 0;
+		int value = sum[N] / 4;
+		dp[0][0] = 1;
+		for (int idx = 1; idx <= N; idx++) {
+			dp[idx][0] = 1;
 
-		if (sum[N] == 0) { // 누적합이 0이라면 수열의 끝을 제외한 누적합이 0인 모든 지점 중 3개를 조합
-			long zeroCount = 0;
-			for (int i = 1; i <= N; i++) {
-				if (sum[i] == 0)
-					zeroCount++;
-			}
+			for (int multi = 1; multi < 4; multi++) {
+				dp[idx][multi] = dp[idx - 1][multi];
 
-			answer = (zeroCount - 1) * (zeroCount - 2) * (zeroCount - 3) / 6;
-		} else { // 누적합이 4의 배수라면 나와야 할 값은 누적합을 4로 나눈 수
-			dp[0] = 1;
-			int value = sum[N] / 4;
-
-			for (int i = 1; i <= N; i++) {
-				int type = sum[i] / value;
-
-				// value의 배수(1 ~ 4)가 아니라면 불가능한 구간
-				if (sum[i] % value != 0 || type < 1 || type > 4) {
-					continue;
+				if (value * multi == sum[idx]) {
+					dp[idx][multi] += dp[idx - 1][multi - 1];
 				}
-
-				dp[type] += dp[type - 1];
 			}
-
-			answer = dp[4];
 		}
 
-		System.out.println(answer);
+		System.out.println(dp[N - 1][3]);
 	}
 }
