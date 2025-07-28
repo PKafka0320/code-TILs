0daa45a223fedbfd7230299f67c1b417fcbafcca
PKafka <66552990+PKafka0320@users.noreply.github.com>
2025-07-28 18:33:27 +0900

    [Gold II] Title: 나누기, Time: 184 ms, Memory: 22528 KB, Score: 65 point -BaekjoonHub

--- 백준/Gold/21757. 나누기/나누기.java
+++ 백준/Gold/21757. 나누기/나누기.java
@@ -6,39 +6,57 @@ import java.util.StringTokenizer;
 public class Main {
 	static int N;
 	static int[] arr, sum;
+	static long[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		N = Integer.parseInt(br.readLine());
-		// arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		arr = new int[N];
-		sum = new int[N];
+		arr = new int[N + 1];
+		sum = new int[N + 1];
+		dp = new long[5];
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 		}
 
-		sum[0] = arr[0];
-		for (int i = 1; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			sum[i] = sum[i - 1] + arr[i];
 		}
 
-		long answer = 0;
-		for (int i = 0; i < N - 3; i++) {
-			for (int j = i + 1; j < N - 2; j++) {
-				for (int k = j + 1; k < N - 1; k++) {
-					int sum1 = sum[i];
-					int sum2 = sum[j] - sum[i];
-					int sum3 = sum[k] - sum[j];
-					int sum4 = sum[N - 1] - sum[k];
+		// 누적합이 4의 배수가 아니라면 불가능
+		if (sum[N] % 4 != 0) {
+			System.out.println(0);
+			return;
+		}
 
-					if (sum1 == sum2 && sum2 == sum3 && sum3 == sum4) {
-						answer++;
-					}
-				}
+		long answer = 0;
+
+		if (sum[N] == 0) { // 누적합이 0이라면 수열의 끝을 제외한 누적합이 0인 모든 지점 중 3개를 조합
+			long zeroCount = 0;
+			for (int i = 1; i <= N; i++) {
+				if (sum[i] == 0)
+					zeroCount++;
 			}
+
+			answer = (zeroCount - 1) * (zeroCount - 2) * (zeroCount - 3) / 6;
+		} else { // 누적합이 4의 배수라면 나와야 할 값은 누적합을 4로 나눈 수
+			dp[0] = 1;
+			int value = sum[N] / 4;
+
+			for (int i = 1; i <= N; i++) {
+				int type = sum[i] / value;
+
+				// value의 배수(1 ~ 4)가 아니라면 불가능한 구간
+				if (sum[i] % value != 0 || type < 1 || type > 4) {
+					continue;
+				}
+
+				dp[type] += dp[type - 1];
+			}
+
+			answer = dp[4];
 		}
 
 		System.out.println(answer);
